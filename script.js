03d00ac754ae3505fe1e196c0f1e856eca332d2e
mikep139 <puckett.mike@pm.me>
2026-03-01 18:07:05 +0300

    add persistent localstorage js to track dark mode state on reload

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const body = document.querySelector("body");
 const button = document.querySelector("#dark-mode-controls");
-button.addEventListener("change", toggleDarkMode);
 
 function toggleDarkMode() {
     body.classList.toggle("dark-mode");
+    if (body.classList.contains("dark-mode")) {
+        localStorage.setItem("darkMode", "dark-mode");
+    } else {
+        localStorage.setItem("darkMode", null);
+    }
 }
+
+function checkDarkMode() {
+    if (localStorage.getItem("darkMode") === "dark-mode"){ 
+        body.classList.add("dark-mode");
+        button.checked = true;
+    } else {
+        body.classList.remove("dark-mode");
+        button.checked = false;
+    }
+}
+button.addEventListener("change", toggleDarkMode);
+document.addEventListener("DOMContentLoaded", checkDarkMode);
